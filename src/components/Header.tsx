7e56ce0a7c0d52a6139cf8f9931b7549c2ca273a
meet.rakholiya.w1 <meet.rakholiya.w1@gmail.com>
2024-06-15 11:08:00 +0000

    start working on home page

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="fixed top-0 w-full z-50 bg-white shadow-md">
+      <div className="flex justify-between items-center px-4 py-4">
+        <div className="logo">{/* Logo here */}</div>
+        <div className="hidden md:flex items-center">
+          <Link href="/#" className="mr-4">
+            Home
+          </Link>
+          <Link href="/#" className="mr-4">
+            About Us
+          </Link>
+          <Link href="/#" className="mr-4">
+            Contact
+          </Link>
+          <Link href="/#" className="mr-4">
+            Services
+          </Link>
+        </div>
+        <div className="md:hidden">{/* <MenuBar /> */}</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
